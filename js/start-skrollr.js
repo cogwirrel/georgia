7b9dc6137b777b01b30e25ccec4384bf8e39297c
Jack Stevenson <cogwirrel@gmail.com>
2014-07-13 12:49:25 +0100

    Scroll kind of works

--- js/start-skrollr.js
+++ js/start-skrollr.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   // initialize skrollr if the window width is large enough
   if ($(window).width() > 1024) {
   	console.log("Start skrollr");
-    window.skrl = skrollr.init({forceHeight: false,smoothScrolling: true, smoothScrollingDuration: 1500});
+    window.skrl = skrollr.init({forceHeight: false,smoothScrolling: true, smoothScrollingDuration: 200});
   }
 });
